648e70caa911d8335f11be82b65d55054746028c
Solomon <ayomidebanjo02@gmail.com>
2024-10-14 15:34:13 +0100

    first commit

---
+++ src/main/java/solo/CloudWatchApplication.java
@@ -0,0 +1,13 @@
+package solo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudWatchApplication.class, args);
+	}
+
+}
