319b9c3114668634b47b8ef64e73ff401a717b32
Banjo Solomon <151349000+banjoSolomon@users.noreply.github.com>
2024-10-14 22:22:52 +0100

    build

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,7 @@ COPY . .
 RUN mvn -B clean package -DskipTests
 FROM openjdk:17
 COPY --from=build target/*.jar Cloud-Watch.jar
+RUN addgroup --system appgroup && adduser --system --ingroup appgroup appuser
+USER appuser
+EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "-Dserver.port=8080", "Cloud-Watch.jar"]
--- src/main/java/solo/service/UserServiceImpl.java
+++ src/main/java/solo/service/UserServiceImpl.java
@@ -15,6 +15,17 @@ public class UserServiceImpl implements UserService{
         this.userRepository = userRepository;
     }
 
+    public void registerUserUsingFibonacci(int number , RegisterRequest request){
+        int num = 0 , num2 = 1;
+        for(int count = 0 ; count < number; count++){
+            registerUser(request);
+            int nextNumber = num + num2;
+            num = num2;
+            num2 = nextNumber;
+            System.out.print("Register User with fibonacci : " + num2);
+        }
+    }
+
     @Override
     public RegisterRequestResponse registerUser(RegisterRequest request) {
         User user = new User();
